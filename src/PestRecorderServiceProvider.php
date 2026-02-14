ca8bf95df4d893ce4e13efc8d6bbafaebc38ef5a
Johan Montenij <johan@comfycoders.nl>
2026-02-14 13:52:42 +0100

    Cleanup

--- src/PestRecorderServiceProvider.php
+++ src/PestRecorderServiceProvider.php
@@ -12,16 +12,9 @@ class PestRecorderServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-pest-recorder')
             ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_laravel_pest_recorder_table')
             ->hasCommand(PestRecorderCommand::class);
     }
 }
